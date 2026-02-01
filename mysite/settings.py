b41b23a7aefff3149f7bfdb21ccc5717c0b70c21
Mhd <mohammad.issa@justexperts.de>
2026-02-01 11:30:04 +0300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+test=1
